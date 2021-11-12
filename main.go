eb66069dca3bf9340cf759a14cbb3bf6972f4249
Yu Zhang <505039426@qq.com>
2021-11-12 11:08:23 +0800

    Modify port

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"flag"
 	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+var (
+	port = flag.Int("port", 8888, "Port of server.")
 )
 
 func main() {
@@ -9,8 +15,8 @@ func main() {
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 	router.Static("/", "./pic")
-	err := router.Run(":8080")
+	err := router.Run(":" + strconv.Itoa(*port))
 	if err != nil {
-		return 
+		return
 	}
 }
