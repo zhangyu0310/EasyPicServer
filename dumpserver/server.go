cef1a42f725531438bd4b630efd838a5f52cd54c
YuZhang <505039426@qq.com>
2021-11-13 22:10:02 +0800

    Add picture dump feature.

---
+++ dumpserver/server.go
@@ -0,0 +1,49 @@
+package dumpserver
+
+import (
+	"context"
+	"easyPicServer/config"
+	"easyPicServer/picdump"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+	"os"
+	"strconv"
+)
+
+type dumpServer struct{}
+
+func (ds *dumpServer) SendPic(ctx context.Context, req *picdump.PicRequest) (*picdump.PicReply, error) {
+	var reply picdump.PicReply
+	reply.Message = "Success."
+	picFile, err := os.OpenFile("./pic/"+req.GetPicName(), os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		reply.Message = "Open pic file failed."
+		return &reply, err
+	}
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(picFile)
+	return &reply, nil
+}
+
+func Run() {
+	cfg := config.GetGlobalConfig()
+	listen, err := net.Listen("tcp", ":"+strconv.Itoa(cfg.DumpPort))
+	if err != nil {
+		fmt.Println("Dump server start failed.", err)
+		os.Exit(1)
+	}
+	server := grpc.NewServer()
+	picdump.RegisterCourierServer(server, &dumpServer{})
+
+	reflection.Register(server)
+
+	go func() {
+		err = server.Serve(listen)
+		if err != nil {
+			fmt.Println("Dump Server run failed.", err)
+		}
+	}()
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"easyPicServer/config"
+	"easyPicServer/dumpserver"
 	"flag"
 	"github.com/gin-gonic/gin"
 	"os"
@@ -8,8 +10,15 @@ import (
 )
 
 var (
-	port = flag.Int("port", 8888, "Port of server.")
+	webPort  = flag.Int("web-port", 8888, "Port of web server")
+	dumpPort = flag.Int("dump-port", 9999, "Port of dump server")
 )
+
+// cmdConfigSetToGlobal store command config to global config.
+func cmdConfigSetToGlobal(cfg *config.Config) {
+	cfg.WebPort = *webPort
+	cfg.DumpPort = *dumpPort
+}
 
 func main() {
 	help := flag.Bool("help", false, "show the usage")
@@ -18,11 +27,14 @@ func main() {
 		flag.Usage()
 		os.Exit(0)
 	}
+	// Start dump server
+	dumpserver.Run()
+	// Start web server
 	router := gin.Default()
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 	router.Static("/", "./pic")
-	err := router.Run(":" + strconv.Itoa(*port))
+	err := router.Run(":" + strconv.Itoa(config.GetGlobalConfig().WebPort))
 	if err != nil {
 		return
 	}
